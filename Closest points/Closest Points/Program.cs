7499064ae73a433009889ddb7d1bc7e4719840cc
Kirluu <kirluu4thewin@gmail.com>
2016-09-19 11:46:25 +0200

    Working solution, non-optimized

--- Closest points/Closest Points/Program.cs
+++ Closest points/Closest Points/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Closest_Points
 {
@@ -12,95 +13,120 @@ namespace Closest_Points
         public double X { get; set; }
         public double Y { get; set; }
         public string Name { get; set; }
+
+        public Point(double x, double y, string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, X: {1}, Y: {2}", Name, X, Y);
+        }
     }
     public class Program
     {
-        private static string inputFile = "data\\";
-
-        public static List<Point> Input { get; set; }
+        public static List<Point> Input { get; set; } = new List<Point>();
         //public static List<Point> SortedByX { get; set; }
         //public static List<Point> SortedByY { get; set; }
+
+        private static bool fullExection = true;
         public static void Main(string[] args)
         {
-            // TODO: Parsing
-            Console.WriteLine("write the name of a file in the data subfoler. FX: \"a280 - tsp.txt\"");
-            inputFile += Console.ReadLine();
-            var fileName = args[1];
-            var currentDir = Environment.CurrentDirectory;
-
-            var fileContents =
-                File.OpenText(Path.Combine(currentDir, string.Format("\\closest-points\\data\\{0}", inputFile)));
-
-            Parse();
-            var input = Input.OrderBy(p => p.X).ToList();
-            double result = DoWork(input);
-        }
-
-        private static void Parse()
-        {
-            System.IO.StreamReader file =
-                new System.IO.StreamReader(inputFile);
-            
-            string line;
-            while ((line = file.ReadLine()) != null)
+            if (fullExection)
             {
-
-
-
-                System.Console.WriteLine(line);
-                
+                var directoryPath = Path.Combine(Path.Combine(Environment.CurrentDirectory, @"..\..\..\"), @"closest-points\data\");
+                var files = Directory.GetFiles(directoryPath);
+                foreach (var file in files)
+                {
+                    CalculateResultForFile(Path.GetFileName(file), file);
+                    Input.Clear();
+                }
             }
+            else
+            {
+                string fileName;
+                if (args.Length > 0 && args[0].Length > 0)
+                {
+                    fileName = args[0];
+                }
+                else
+                {
+                    Console.WriteLine("Write the name of a file in the data subfolder. FX: \"a280 - tsp.txt\"");
+                    fileName = Console.ReadLine();
+                }
+                var fullPath = Path.Combine(Path.Combine(Environment.CurrentDirectory, @"..\..\..\"),
+                    string.Format(@"closest-points\data\{0}", fileName));
 
-            file.Close();
+                CalculateResultForFile(fileName, fullPath);
+            }
+            
+            Console.ReadLine();
         }
 
-        private static void Overall()
+        private static void CalculateResultForFile(string fileName, string fullPath)
         {
+            var contentArray = File.ReadAllLines(fullPath);
+
+            Parse(contentArray);
+            
+            var input = Input.OrderBy(p => p.X).ToList();
+            var result = DoWork(input);
+
+            Console.WriteLine(fileName + ": " + Input.Count + ", " + result);
+        }
+
+        private static void Parse(string[] contents)
+        {
+            foreach (string line in contents)
+            {
+                var trimmedLine = line.TrimStart().TrimEnd().Split(null);
+                var values = trimmedLine.ToList().Where(r => r.Length != 0).ToList();
+                double dummy;
+                if (values.Count != 3 || !double.TryParse(values[1], out dummy)) { continue; }
+                var x = (double) decimal.Parse(values[1], NumberStyles.Any, CultureInfo.InvariantCulture);
+                var y = (double) decimal.Parse(values[2], NumberStyles.Any, CultureInfo.InvariantCulture);
+                var point = new Point(x, y, values[0]);
+                Input.Add(point);
+            }
+            if (fullExection) return;
+            // Print if just one file
+            foreach (var point in Input)
+            {
+                Console.WriteLine(point);
+            }
         }
 
         private static double DoWork(List<Point> input)
         {
-            var dist = 0.0;
-            var delta = 0.0;
-            if (input.Count <= 3) //we assume there can never be less than 2 points in input
+            double dist;
+            double delta;
+            if (input.Count < 4) //we assume there can never be less than 2 points in input
             {
-                
-                for (int i = 0; i < input.Count; i++)
-                {
-                    for (int j = 0; j < input.Count; j++)
-                    {
-                        if (i == j) continue; //don't compare the point with itself
-                        dist =
-                            Math.Sqrt(Math.Pow(input[i].X - input[j].X, 2) +
-                                      Math.Pow(input[i].Y - input[j].Y, 2));
-                        if (i == 0 && j == 1) delta = dist; //set the first delta so it is not 0
-
-                        else if (dist < delta)
-                        {
-                            delta = dist;
-                        }
-                    }
-                }
-                return delta;
+                return GetResultForLessThanFourPoints(input);
             }
             var halfSize = input.Count/2;
-            var part1 = input.GetRange(0, halfSize);
-            var part2 = input.GetRange(halfSize, input.Count - halfSize);
-            var res1 = DoWork(part1);
-            var res2 = DoWork(part2);
-            delta = res1 <= res2 ? res1 : res2;
+            var res1 = DoWork(input.GetRange(0, halfSize));
+            var res2 = DoWork(input.GetRange(halfSize, input.Count - halfSize));
+            delta = Math.Min(res1, res2);
             var midElem = input[halfSize];
             var ySortedWithinDelta = input.Where(p => (midElem.X - p.X <= delta && midElem.X - p.X >= 0)
                                                       || (p.X - midElem.X <= delta && p.X - midElem.X >= 0)).OrderBy(p => p.Y).ToList();
-            var part1SortY = part1.Where(p => midElem.X - p.X <= delta).OrderBy(p => p.Y);
-            var part2SortY = part2.Where(p => p.X - midElem.X <= delta).OrderBy(p => p.Y);
-            var result = delta;
+
+            if (ySortedWithinDelta.Count == 1) return delta;
+
             for (int i = 0; i < ySortedWithinDelta.Count; i++)
             {
                 var pt = ySortedWithinDelta[i];
-                var comparedTo = ySortedWithinDelta.GetRange(i - 10 < 0 ? 0 : i - 10,
-                    i + 10 > ySortedWithinDelta.Count - 1 ? ySortedWithinDelta.Count - 1 : i + 10);
-                    //20 - i >= ySortedWithinDelta.Count ? ySortedWithinDelta.Count - 1 : 20 - i);
+                var ySortedWithoutPt = new List<Point>(ySortedWithinDelta);
+                ySortedWithoutPt.RemoveAt(i);
+
+                var startIndex = Math.Max(0, i - 10);
+                var endIndex = Math.Min(ySortedWithoutPt.Count - 1, i + 10);
+                var length = endIndex - startIndex;
+                var comparedTo = ySortedWithoutPt.GetRange(startIndex, length);
                 foreach (var cmpPt in comparedTo)
                 {
                     dist = Math.Sqrt(Math.Pow(cmpPt.X - pt.X, 2) + Math.Pow(cmpPt.Y - pt.Y, 2));
@@ -109,11 +135,30 @@ namespace Closest_Points
                         delta = dist;
                     }
                 }
-
             }
             return delta;
         }
 
+        private static double GetResultForLessThanFourPoints(List<Point> input)
+        {
+            double delta = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input.Count; j++)
+                {
+                    if (i == j) continue; //don't compare the point with itself
+                    var dist =
+                        Math.Sqrt(Math.Pow(input[i].X - input[j].X, 2) +
+                                  Math.Pow(input[i].Y - input[j].Y, 2));
+                    if (i == 0 && j == 1) delta = dist; //set the first delta so it is not 0
 
+                    else if (dist < delta)
+                    {
+                        delta = dist;
+                    }
+                }
+            }
+            return delta;
+        }
     }
 }
