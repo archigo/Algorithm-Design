825c12006fa42e3d62b265161488fde70b88888c
archigo <aspo-ja@hotmail.com>
2016-09-26 08:36:09 +0200

    started parse

---
+++ gorilla/Gorilla/Gorilla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorilla
+{
+    class Program
+    {
+        public List<string> Type { get; set; }
+
+        static void Main(string[] args)
+        {
+        }
+
+
+        private static void Parse()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteLines2.txt");
+
+            
+        }
+    }
+}
