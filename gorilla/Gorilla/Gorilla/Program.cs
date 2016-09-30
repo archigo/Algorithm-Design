01d95eeabf6c157ba4b9e5dc0ca392656ac2b986
Claudiu Bele <bele.claudiu@yahoo.ro>
2016-09-30 19:39:32 +0200

    More ifs where you can put breakpoints, commented new approach to recursion, maybe we can remove the stars.

--- gorilla/Gorilla/Gorilla/Program.cs
+++ gorilla/Gorilla/Gorilla/Program.cs
@@ -21,32 +21,24 @@ namespace Gorilla
         public static string Second { get; set; }
         public static Dictionary<Tuple<int, int>, Tuple<int, string>> Cache { get; set; } = new Dictionary<Tuple<int, int>, Tuple<int, string>>();
 
-
+        public static int CountCaches { get; set; }
         static void Main(string[] args)
         {
-            First = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH";
-            Second = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFKLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH";
+            Second = "XGGTLAIQAQGDLTLAQKKIVRKTWHQLMRNKTSFVTDVFIRIFAYDPSAQNKFPQMAGMSASQLRSSRQMQAHAIRVSSIMSEYVEELDSDILPELLATLARTHDLNKVGADHYNLFAKVLMEALQAELGSDFNEKTRDAWAKAFSVVQAVLLVKHG";
+            First = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFKLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH";
             if (args.Length == 2)
             {
-
-                if (args[0].Length >= args[1].Length)
-                {
-                    First = args[0];
-                    Second = args[1];
-                }
-                else
-                {
-                    First = args[1];
-                    Second = args[0];
-                }
-                var stars = First.Length - Second.Length;
-                Second = new StringBuilder().Append('*', stars).Append(Second).ToString();
-
                 First = args[0];
                 Second = args[1];
 
 
             }
+            //var stars = Math.Abs(First.Length - Second.Length);
+
+            //if (First.Length < Second.Length)
+            //    First = new StringBuilder().Append('*', stars).Append(First).ToString();
+            //else
+            //    Second = new StringBuilder().Append('*', stars).Append(Second).ToString();
 
             // Parse Cost-Matrix
             var contents = File.ReadAllLines(Path.Combine(Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\"),
@@ -54,75 +46,119 @@ namespace Gorilla
             ParseCostMatrix(contents);
 
             // Get some input
-            
+
             // Result: Should be "K-AK" but is "KAK*"
             // Do work given last valid indexes
             var res = DoWork(First.Length - 1, Second.Length - 1);
             Console.WriteLine("Based on: ");
             Console.WriteLine("Cost: {0}\n{1}", res.Item1, res.Item2);
 
-            Console.ReadLine();
+             Console.ReadLine();
         }
-
+        
         // "OPT"
         private static Tuple<int, string> DoWork(int idx1, int idx2)
         {
-            Tuple<int, string> result;
-            if (Cache.TryGetValue(Tuple.Create(idx1, idx2), out result)) return result;
-
+            if (idx1 == 0 && idx2 == 0)
+            {
+            }
+                Tuple<int, string> result;
+            if (Cache.TryGetValue(Tuple.Create(idx1, idx2), out result))
+            {
+                CountCaches++;
+                return result;
+            }
             // Check out of bounds
             if (idx1 == -1 || idx2 == -1) return Tuple.Create(0, ""); // "****"
 
             var char1 = First[idx1];
             var char2 = Second[idx2];
 
+
             int costIdx1;
-            if(!IdToIndex.TryGetValue(char1.ToString(), out costIdx1)) throw new Exception("Oh no, we failed!");
+            if (!IdToIndex.TryGetValue(char1.ToString(), out costIdx1)) throw new Exception("Oh no, we failed!");
             int costIdx2;
             if (!IdToIndex.TryGetValue(char2.ToString(), out costIdx2)) throw new Exception("Oh no, we failed!");
-            
+
             var minusCost = -4; // Could look up in matrix under "*" and char2
             var exchangeCost = CostMatrix[costIdx1][costIdx2];
+             if (idx1 <= 5 && idx2 <= 5)
+            {
 
+            }
             if (idx1 == 0 && idx2 == 0)
             {
                 var res = exchangeCost >= minusCost
                     ? Tuple.Create(exchangeCost, "" + char2)
                     : Tuple.Create(minusCost, "*");
-                Cache.Add(Tuple.Create(idx1, idx2), res);
+                if(Cache.ContainsKey(Tuple.Create(idx1,idx2)))
+                {
+                    Tuple<int,String> tempRes;
+                    Cache.TryGetValue(Tuple.Create(idx1, idx2), out tempRes);
+                    if (tempRes != null && (tempRes.Item1 != res.Item1 || !tempRes.Item2.Equals(res.Item2)))
+                        Console.WriteLine(res.Item1 + " vs cached " + tempRes);
+                }
+                else
+                    Cache.Add(Tuple.Create(idx1, idx2), res);
                 //Console.WriteLine("Cached: ({0}, {1}) --> {2}, {3}", idx1, idx2, res.Item1, res.Item2);
                 return res;
             }
+
+
+
+
+            //var minus1 = idx2 -1 >0  && idx1 > 0 ? DoWork(idx1, idx2 - 1) : new Tuple<int, string>(-10000, "*");
+            //var replace = idx2 > 0 && idx1 > 0 ? DoWork(idx1 - 1, idx2 - 1) : new Tuple<int, string>(-10000, "*");
+            //var minus2 = idx1 - 1 > 0 && idx2 > 0 ? DoWork(idx1 - 1, idx2) : new Tuple<int, string>(-10000, "*");
 
             var minus1 = DoWork(idx1, idx2 - 1);
             var replace = DoWork(idx1 - 1, idx2 - 1);
             var minus2 = DoWork(idx1 - 1, idx2);
 
+
             //if (minus1.Item2.Equals("out")) minus1 = Tuple.Create(-999, "out");
             //if (minus2.Item2.Equals("out")) minus2 = Tuple.Create(-999, "out");
             //if (replace.Item2.Equals("out")) replace = Tuple.Create(-999, "out");
 
-            var best =  Math.Max(minus1.Item1+minusCost, Math.Max(replace.Item1+exchangeCost, minus2.Item1+minusCost));
+            //if (idx1 == 0 && idx2 == 1)
+            //Console.Write("debug");
+
+
+            var best = Math.Max(minus1.Item1 + minusCost, Math.Max(replace.Item1 + exchangeCost, minus2.Item1 + minusCost));
             string part;
-            if (minus1.Item1 == best)
-            {
-                part = minus1.Item2 + "*";
-                //Console.WriteLine("minus 1! (Iterating string 2)");
-            }
-            else if (replace.Item1+exchangeCost == best)
+             if (replace.Item1 + exchangeCost == best)
             {
                 part = replace.Item2 + char2;
                 //Console.WriteLine("Exchanging {0} with {1}", char1, char2);
             }
-            else
+            else if(minus1.Item1 + minusCost == best)
+            {
+                part = minus1.Item2 + "*";
+                //Console.WriteLine("minus 1! (Iterating string 2)");
+            }
+            else if (minus2.Item1 + minusCost == best)
             {
                 part = minus2.Item2 + "*";
                 //Console.WriteLine("minus 2! (Iterating string 1)");
             }
+            else throw new Exception("Oh no, we failed!");
+
+            if (idx1 <= 15 && idx2 <= 5)
+            {
+                // for breakpoints to inspect first few chars
+            }
 
             // Cache idx + "part"
             var costAndPart = Tuple.Create(best, part);
-            Cache.Add(Tuple.Create(idx1, idx2), costAndPart);
+            if (Cache.ContainsKey(Tuple.Create(idx1, idx2)))
+            {
+                Tuple<int, String> tempRes;
+                Cache.TryGetValue(Tuple.Create(idx1, idx2), out tempRes);
+                if (tempRes != null && (tempRes.Item1 != costAndPart.Item1 || !tempRes.Item2.Equals(costAndPart.Item2)))
+                    Console.WriteLine(costAndPart.Item1 + " vs cached " + tempRes);
+            }
+            else
+                Cache.Add(Tuple.Create(idx1, idx2), costAndPart);
             //Console.WriteLine("Cached: ({0}, {1}) --> {2}, {3}", idx1, idx2, best, part);
             return costAndPart;
         }
