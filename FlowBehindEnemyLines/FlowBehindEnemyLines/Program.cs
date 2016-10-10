db67eb0b692d18e23ec7966a1c68c709a3886cd9
Kirluu <kirluu4thewin@gmail.com>
2016-10-10 10:49:44 +0200

    Intermediate commit

--- FlowBehindEnemyLines/FlowBehindEnemyLines/Program.cs
+++ FlowBehindEnemyLines/FlowBehindEnemyLines/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,15 +11,15 @@ namespace FlowBehindEnemyLines
 {
 	public class Edge
 	{
-		public Node From { get; set; }
-		public Node To { get; set; }
+		public int FromId { get; set; }
+		public int ToId { get; set; }
 		public int Capacity { get; set; }
 		public int Flow { get; set; }
 
-		public Edge(Node from, Node to, int capacity)
+		public Edge(int fromId, int toId, int capacity)
 		{
-			From = from;
-			To = to;
+			FromId = fromId;
+			ToId = toId;
 			Capacity = capacity;
 		}
 	}
@@ -27,7 +28,7 @@ namespace FlowBehindEnemyLines
     {
         public readonly int Id;
         public readonly string Name;
-        public List<Edge> Outgoing { get; set; }
+        public List<Edge> Outgoing { get; set; } = new List<Edge>();
 
         public Node(int id, string name)
         {
@@ -53,11 +54,9 @@ namespace FlowBehindEnemyLines
 			{
 				Nodes = Nodes.Select(
 					n =>
-						new Node
+						new Node(n.Id, n.Name)
 						{
-							Id = n.Id,
-							Name = n.Name,
-							Outgoing = n.Outgoing.Select(e => new Edge(e.From, e.To, e.Capacity)).ToList()
+							Outgoing = n.Outgoing.Select(e => new Edge(e.FromId, e.ToId, e.Capacity)).ToList()
 						}).ToList()
 			};
 		}
@@ -69,7 +68,7 @@ namespace FlowBehindEnemyLines
 			{
 				foreach (Edge edge in node.Outgoing)
 				{
-					t += edge.From.Name + " -> " + edge.To.Name + Environment.NewLine;
+					t += Nodes.Find(n => n.Id == edge.FromId).Name + " -> " + Nodes.Find(n => n.Id == edge.ToId).Name + Environment.NewLine;
 				}
 			}
 			return t;
@@ -86,13 +85,13 @@ namespace FlowBehindEnemyLines
 			var residualGraph = graph.Copy();
 
 			var alg = new Algorithm(graph, residualGraph,
-				graph.Nodes.Find(n => n.Name == "ORIGINS"),
-				graph.Nodes.Find(n => n.Name == "DESTINATIONS"));
+				graph.Nodes.Find(n => n.Name == "ORIGINS").Id,
+				graph.Nodes.Find(n => n.Name == "DESTINATIONS").Id);
 
 			alg.DoStuff();
-		}
 
-        
+		    Console.Read();
+		}
     }
 
     public class Path
@@ -115,17 +114,17 @@ namespace FlowBehindEnemyLines
 		public Graph Original { get; set; }
 		public Graph Residual { get; set; }
 
-        public HashSet<Node> MinimumCut { get; set; }
+        public HashSet<Node> MinimumCut { get; set; } = new HashSet<Node>();
 
-        public Node Source { get; set; }
-        public Node Target { get; set; }
+        public int SourceId { get; set; }
+        public int TargetId { get; set; }
 
-		public Algorithm(Graph original, Graph residual, Node source, Node target)
+		public Algorithm(Graph original, Graph residual, int sourceId, int targetId)
 		{
 			Original = original;
 			Residual = residual;
-			Source = source;
-			Target = target;
+			SourceId = sourceId;
+			TargetId = targetId;
 		}
 
         public void DoStuff()
@@ -134,14 +133,32 @@ namespace FlowBehindEnemyLines
 
             // Reset MinimumCut
             MinimumCut.Clear();
-            var path = FindResidualPath(new Path { Nodes = {Source}, Bottleneck = int.MaxValue });
+            var sourceNode = Original.Nodes.Find(n => n.Id == SourceId);
+            var sourceNodeList = new List<Node>();
+            sourceNodeList.Add(sourceNode);
+            var pathStartingPoint = new Path { Nodes = sourceNodeList, Bottleneck = int.MaxValue };
+            var path = FindResidualPath(pathStartingPoint);
+
+#if DEBUG
+            if (path != null)
+            {
+                Console.WriteLine("Path found:");
+                foreach (var node in path.Nodes)
+                {
+                    if (node == path.Nodes.First()) Console.Write(node.Name);
+                    else Console.Write(" --> {0}", node.Name);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Bottleneck: {0}", path.Bottleneck);
+            }
+#endif
 
         }
 
         private Path FindResidualPath(Path path)
         {
             // Check if this path has just reached DESTINATIONS
-            if (path.Nodes.Last().Id == Target.Id) return path;
+            if (path.Nodes.Last().Id == TargetId) return path;
 
             if (path.Nodes.Last().Outgoing.Count == 0)
             {
@@ -153,24 +170,31 @@ namespace FlowBehindEnemyLines
             foreach (var edge in path.Nodes.Last().Outgoing)
             {
                 // Check that edge doesn't lead to already-visited node
-                if (path.Nodes.Exists(n => n.Id == edge.To.Id)) continue;
+                if (path.Nodes.Exists(n => n.Id == edge.ToId)) continue;
+
+                var node = Original.Nodes.Find(n => n.Id == edge.ToId);
 
                 // Update recurisvely used path copy
                 var copy = path.Copy();
-                MinimumCut.Add(edge.To);
+                copy.Nodes.Add(node);
+                if (edge.Capacity == -1)
+                {
+                    // TODO: Handle infinity...
+                }
                 if (edge.Capacity < copy.Bottleneck) copy.Bottleneck = edge.Capacity;
 
                 // Update set of reached Nodes
-                copy.Nodes.Add(edge.To);
+                MinimumCut.Add(node);
 
                 // Store result of recursion
-                resultPaths.Add(FindResidualPath(copy));
+                var resPath = FindResidualPath(copy);
+                if (resPath != null) resultPaths.Add(resPath);
             }
 
             Path res = null;
             foreach (var resPath in resultPaths)
             {
-                if (resPath.Nodes.Last().Id != Target.Id) continue;
+                if (resPath.Nodes.Last().Id != TargetId) continue;
 
                 if (res == null) res = resPath;
                 else if (resPath.Bottleneck > res.Bottleneck) // Want the highest bottleneck
@@ -202,11 +226,7 @@ namespace FlowBehindEnemyLines
 			int n = int.Parse(contentArray[0]);
 			for (int i = 1; i <= n; i++)
 			{ // Parse each node, and place them in the graph
-				graph.Nodes.Add(new Node()
-				{
-					Name = contentArray[i],
-					Id = i - 1
-				});
+				graph.Nodes.Add(new Node(i - 1, contentArray[i]));
 
 				// Test
 				//Console.WriteLine("Node: " + contentArray[i] + ", " + (i - 1));
@@ -230,7 +250,7 @@ namespace FlowBehindEnemyLines
 				Node to = graph.Nodes.Find(node => node.Id == v);
 
 				// Create the edge
-				var edge = new Edge(from, to, c);
+				var edge = new Edge(from.Id, to.Id, c);
 				// Add edge as outgoing on the from node
 				from.Outgoing.Add(edge);
 
